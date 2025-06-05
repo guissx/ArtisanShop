53a6d990413da50493435f98300f5717093266bb
Gustavo Ferreira Cabral <gugacabral03@gmail.com>
2025-06-05 08:34:46 -0300

    Fix users routes bug

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "../src/config/db";
-import UsersRoutes from "../src/routes/auth/UsersArtisanRoutes";
+import UsersRoutes from "../src/routes/auth/UsersRoutes";
 import AuthRoutes from "../src/routes/auth/AuthRoutes";
 import ProductRoutes from "../src/routes/ProductRoutes"
 import UserArtisanRoutes from '../src/routes/auth/UsersArtisanRoutes'
