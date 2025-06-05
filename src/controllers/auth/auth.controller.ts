f4f8b349a881d8134eeab531d64c85c2025a7483
Gustavo Ferreira Cabral <gugacabral03@gmail.com>
2025-06-05 07:58:40 -0300

    Feat Login to both Users

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import User from "../../models/UserModel"
 import Artisan from "../../models/UserModelArtisan";
 
 const JWT_SECRET = process.env.JWT_SECRET || "secret123";
@@ -9,7 +10,16 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
   const { email, password } = req.body;
 
   try {
-    const user = await Artisan.findOne({ email, isDeleted: false });
+        // tenta buscar primeiro como usuário comum
+    let user = await User.findOne({ email });
+    let role = "user";
+
+    if (!user) {
+      // se não encontrar, tenta como artesão
+      user = await Artisan.findOne({ email, isDeleted: false });
+      role = "artisan";
+    }
+
     if (!user) {
       res.status(404).json({ message: "Usuário não encontrado" });
       return;
@@ -23,7 +33,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
     }
 
     const token = jwt.sign(
-      { userId: user._id, email: user.email },
+      { userId: user._id, email: user.email, role},
       JWT_SECRET,
       { expiresIn: "1h" }
     );
